2a02b626d66644acf945fd2a4839299f606161da
Natalie Marques <namqsf@gmail.com>
2023-02-27 09:25:14 -0300

    Add files via upload

---
+++ ex011.js
@@ -0,0 +1,12 @@
+var idade=17
+if (idade>18) {
+    console.log('Voto obrigatório')
+}
+else {
+    if (idade<16) {
+        console.log('Não pode votar')
+    }
+    else {
+        console.log('Voto opicional')
+    }
+}
