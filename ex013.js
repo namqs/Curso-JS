57d092b2f1603f5912e25aa6a14694712d5e729a
Natalie Marques <namqsf@gmail.com>
2023-02-27 09:53:18 -0300

    Switch Case + Dia de acordo com o sistema

---
+++ ex013.js
@@ -0,0 +1,26 @@
+var hoje = new Date ()
+var diasem = hoje.getDay ()
+/* 
+    Dom - 0
+    Seg - 1
+    Ter - 2
+    Qua - 3
+    Qui - 4
+    Sex - 5
+    Sab - 6
+*/
+switch (diasem){
+    case 0: console.log('Hoje é domingo')
+        break
+    case 1: console.log('Hoje é segundouu')
+        break
+    case 2: console.log('Hoje é terçouuu')
+        break
+    case 3: console.log('Hoje é quartouuu')
+        break
+    case 4: console.log('Hoje é quintouuu')
+        break
+    case 5: console.log('Hoje é sextouuuu')
+        break
+    case 6: console.log('Hoje é sabadouuu')
+}
